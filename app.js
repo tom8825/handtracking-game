922e8227a54142aeca750b6f996ca9d6e242f174
tom8825 <tlparker1988@gmail.com>
2019-06-17 14:07:56 +0100

    added canvas overlay for drawing, various changes

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ navigator.getUserMedia =
   navigator.webkitGetUserMedia;
 
 const video = document.querySelector("#video");
-const canvas = document.querySelector("#canvas");
-const context = canvas.getContext("2d");
+const trackCanvas = document.querySelector("#trackCanvas");
+const context = trackCanvas.getContext("2d");
 const audio = document.querySelector("#audio");
 let model;
 
@@ -33,16 +33,32 @@ handTrack.startVideo(video).then(status => {
 
 function runDetection() {
   model.detect(video).then(predictions => {
-    console.log(predictions);
-    if(predictions.length > 0){
-        audio.play();
+    //console.log(predictions);
+    if (predictions.length > 0) {
+      audio.play();
+      //predictions.shift();
+      console.log(predictions);
+      //redraw(((predictions[predictions.length-1]["bbox"][0]+predictions[predictions.length-1]["bbox"][1])/2), ((predictions[predictions.length-1]["bbox"][1]+predictions[predictions.length-1]["bbox"][3])/2));
     }
     requestAnimationFrame(runDetection);
     //view detector
-    //model.renderPredictions(predictions, canvas, context, video);
+    model.renderPredictions(predictions, trackCanvas, context, video);
   });
 }
 
 handTrack.load(modelParams).then(lmodel => {
   model = lmodel;
 });
+
+var drawCanvas = document.getElementById('drawCanvas');
+var c = drawCanvas.getContext('2d');
+
+
+var putLeaves = function() {
+c.beginPath();
+c.arc(300,300,30,0,Math.PI*2,false);
+c.fillStyle ="#d57b85"
+c.fill
+}
+
+drawCanvas.addEventListner('mousedown', putLeaves)
